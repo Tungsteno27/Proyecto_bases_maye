f53c72c4bff3997424d327f8c3525aab88b0fa61
Tungs <Tungs@Aegon-l>
2026-02-18 19:36:05 -0700

    primer Commit lets go

---
+++ src/main/java/com/mayepizzas/MayePizzas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mayepizzas;
+
+/**
+ *
+ * @author Tungs
+ */
+public class MayePizzas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
