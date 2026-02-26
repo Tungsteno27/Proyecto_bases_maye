41f7230c9fe356ff3f948caa33964ab097932408
Tungsteno27 <tungsteno5027@gmail.com>
2026-02-25 23:42:55 -0700

    Update evento_20min.sql
    esta si funciona

--- evento_20min.sql
+++ evento_20min.sql
@@ -1,11 +1,11 @@
-use mayepizzas;
-
+drop event verificarPedidosNoReclamados;
 SET GLOBAL event_scheduler = ON;
-
 CREATE EVENT verificarPedidosNoReclamados
 ON SCHEDULE EVERY 1 MINUTE
 DO
-UPDATE Pedidos
-SET estado = 'No Reclamado'
-WHERE estado = 'Listo'
-AND fechaCreacion <= NOW() - INTERVAL 20 MINUTE;
+UPDATE Pedidos p
+SET p.estado = 'No Reclamado'
+WHERE p.estado = 'Listo'
+AND p.idCliente IS NULL
+AND p.fechaCreacion <= NOW() - INTERVAL 20 MINUTE;
+;
